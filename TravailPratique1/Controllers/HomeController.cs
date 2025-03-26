a89c5bea6815aaa119f0061f1ef390e2024d00b6
Maertens <105832204+maertens-ca@users.noreply.github.com>
2025-03-26 15:43:52 -0400

    Creation des Pages + Menu
    Plus long que je pensais mais la logique commence à rentrer

--- TravailPratique1/Controllers/HomeController.cs
+++ TravailPratique1/Controllers/HomeController.cs
@@ -8,5 +8,42 @@ namespace TravailPratique1.Controllers
         {
             return View();
         }
+
+        public IActionResult Inscription()
+        {
+            return View();
+        }
+        public IActionResult Authentification()
+        {
+            return View();
+        }
+        public IActionResult Commandes()
+        {
+            return View();
+        }
+        public IActionResult Erreur404()
+        {
+            return View();
+        }
+        public IActionResult Factures()
+        {
+            return View();
+        }
+        public IActionResult Paiement()
+        {
+            return View();
+        }
+        public IActionResult Panier()
+        {
+            return View();
+        }
+        public IActionResult Produits()
+        {
+            return View();
+        }
+        public IActionResult Profil()
+        {
+            return View();
+        }
     }
 }
