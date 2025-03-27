b7b5ac8d9ef07d085d1e0faa839b716a7ad8259d
Enrik-B <bere0076@uqar.ca>
2025-03-27 16:01:02 -0400

    Début Factures et Commandes + template pour les deux, ajout détails classe User

--- TravailPratique1/Models/User.cs
+++ TravailPratique1/Models/User.cs
@@ -2,5 +2,9 @@
 {
     public class User
     {
+        public int userId { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public string email { get; set; }
     }
 }
