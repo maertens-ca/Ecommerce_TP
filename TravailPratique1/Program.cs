677e15ad2d6ec9de1fe0c060dae5904afbbae616
Maertens <105832204+maertens-ca@users.noreply.github.com>
2025-03-31 12:14:18 -0400

    ajout quelques package

--- TravailPratique1/Program.cs
+++ TravailPratique1/Program.cs
@@ -8,13 +8,28 @@ namespace TravailPratique1
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-            builder.Services.AddMvc(option => option.EnableEndpointRouting = false);
+            builder.Services
+                .AddMvc(option => option.EnableEndpointRouting = false)
+                .AddNewtonsoftJson(option => option.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+            builder.Services.AddSwaggerGen();
+
+            //Préparation Stripe
+            builder.Services.Configure<StripeSettings>(builder.Configuration.GetSection("Stripe"));
+            StripeConfiguration.ApiKey = builder.Configuration["Stripe:SecretKey"];
+
             var app = builder.Build();
+
             
 
-            // Préparation Stripe
-            //builder.Services.Configure<StripeSettings>(builder.Configuration.GetSection("Stripe"));
-            //StripeConfiguration.ApiKey = builder.Configuration["Stripe:SecretKey"];
+            
+
+            //Condig Swagger
+            
+            if(app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TravailPratique1 v1"));
+            }
 
             // Création routes
             app.UseMvc(routes =>
